84d3df8961e2762fae98da286a99e67cb749f818
yananazarenko417 <74686381+dnd1993@users.noreply.github.com>
2024-02-27 16:08:02 -0500

    [feat] Adds the Authenticator component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports.js';
+Amplify.configure(awsconfig);
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Authenticator>
+      <App />
+    </Authenticator>
   </React.StrictMode>,
 )
