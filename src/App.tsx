e2191c73d7aa419221ce0357622a07cefb80d6ae
yananazarenko417 <74686381+dnd1993@users.noreply.github.com>
2024-03-07 14:13:32 -0500

    [feat] Saves a new user to DB on sign up

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useAuthenticator } from '@aws-amplify/ui-react';
 
 function App() {
-
+  const { signOut } = useAuthenticator((context) => [context.signOut]);
   return (
     <>
+      <button onClick={signOut}>Sign Out</button>
       <h1>Good Reads Looking App</h1>
     </>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,56 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
+import {createUser} from './graphql/mutations.ts';
+
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from 'aws-amplify';
-import awsconfig from './aws-exports.js';
-Amplify.configure(awsconfig);
+import { signUp } from 'aws-amplify/auth';
+import { generateClient } from 'aws-amplify/api';
+import config from './amplifyconfiguration.json';
+Amplify.configure(config);
 
+const client = generateClient();
+
+export default function AuthenticatorWithEmail() {
+  const services = {
+    async handleSignUp(formData) {
+      console.log('form data', formData);
+      let { username, password, options } = formData;
+      username = username.toLowerCase();
+      options.userAttributes.email = options.userAttributes.email.toLowerCase();
+      
+      const newUser = await client.graphql({
+        query: createUser,
+        variables: {
+          input: {
+            username,
+            email: options.userAttributes.email
+          }
+        }
+      })
+      console.log(newUser)
+      return await signUp({
+        username,
+        password,
+        options,
+        autoSignIn: {
+          enabled: true,
+        },
+      })
+    }
+  }
+
+  return (
+    <Authenticator services={services} initialState='signUp'>
+      <App />
+    </Authenticator>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Authenticator>
-      <App />
-    </Authenticator>
+    <AuthenticatorWithEmail />
   </React.StrictMode>,
 )
